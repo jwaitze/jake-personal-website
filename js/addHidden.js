509029b839b54320a4b0f8a6ce27f50874994e5d
Jake Waitze <jake@waitze.net>
2016-07-08 04:49:17 -0400

    First additions of mobile menu

--- js/addHidden.js
+++ js/addHidden.js
@@ -3,13 +3,23 @@ var myPhone = '8'+'6'+'0'+' '+'5'+'0'+'5'+' '+'9'+'6'+'9'+'4';
 var myEmail = 'Jake'+String.fromCharCode(64)+'Waitze'+'.net'
 
 function addHiddenLinks() {
-	document.getElementById('hiddenPhoneLink').href += myPhone;
-	document.getElementById('hiddenEmailLink').href += myEmail;
+	var elements =  document.getElementsByClassName('hiddenPhoneLink');
+	for(var i = 0; i < elements.length; i++)
+		elements[i].href  += myPhone;
+
+	elements =  document.getElementsByClassName('hiddenEmailLink');
+	for(var i = 0; i < elements.length; i++)
+		elements[i].href  += myEmail;
 }
 
 function addHiddenHTML() {
-	document.getElementById('hiddenPhone').innerHTML += myPhone;
-	document.getElementById('hiddenEmail').innerHTML += myEmail;
+	var elements =  document.getElementsByClassName('hiddenPhone');
+	for(var i = 0; i < elements.length; i++)
+		elements[i].innerHTML  += myPhone;
+
+	elements =  document.getElementsByClassName('hiddenEmail');
+	for(var i = 0; i < elements.length; i++)
+		elements[i].innerHTML  += myEmail;
 }
 
 function showMainBody() {
