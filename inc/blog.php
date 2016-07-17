930fa8276d6287259298617734000254675b8719
Jake Waitze <jake@waitze.net>
2016-07-17 02:09:50 -0400

    Making blog post logic, next to make page changing and post limiting. in the future, make it so posts are listed chronologically with most recent first.

--- inc/blog.php
+++ inc/blog.php
@@ -7,20 +7,32 @@
 
 	$path = "posts";
 
-	if(isset($_GET['p'])) { // direct link to post
-		$filepath = $path . '/' . urldecode($_GET['p']) . '.php';
+	if(isset($_GET['post'])) { // direct link to post
+		$filepath = $path . '/' . urldecode($_GET['post']) . '.php';
 		if(!file_exists($filepath))
 			include("../inc/no_post_found.php");
-		else
-			echo file_get_contents($filepath);
+		else {
+			$page_filepath = "";
+			include($filepath);
+		}
 
-		return; // exit this included file
+		echo "<a href=\"../blog\"><<< Go to Blog <<<</a>";
+
+		return; // exit just this included file
 	}
 
-	$files = array_diff(scandir($path), array('.', '..')); // omit this dir and prev dir
-	foreach($files as $file)
-		echo file_get_contents($path . '/' . $file);
+	$files = array_diff(scandir($path), array('.', '..')); // omit this dir & prev dir
 
-	// need to add splitting up of X amount of blog posts... consider shrinking them and linking to them directly
+	// to limit the amount of posts shown, this is where i will choose what posts go in the array.
+
+	foreach($files as $file) {
+		$page_filepath = "?post=" . basename($file, ".php");
+		include($path . '/' . $file);
+
+		if($file != end($files)) // add the line if it's not the last entry
+			echo "<hr>";
+	}
+
+	echo "<a href=\"?page=2\"><div class=\"nextPage\">>>> Next Page >>></div></a>";
 
 ?>
