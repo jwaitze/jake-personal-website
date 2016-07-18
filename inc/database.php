7affb5129f8feaad891335969b53861ba4d14322
Jake Waitze <jake@waitze.net>
2016-07-18 04:05:33 -0400

    Fixed mysql charset from utf-8 to utf8mb4

--- inc/database.php
+++ inc/database.php
@@ -4,6 +4,6 @@
 	if($mysqli->connect_errno)
 		die("DB Error (" . $mysqli->connect_error . ")\n");
 
-	$mysqli->set_charset("utf-8");
+	$mysqli->set_charset("utf8mb4"); // use the correct utf-8 charset for mysql...
 	
 ?>
