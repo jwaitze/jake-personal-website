5a2494d166acacb9fe1a08cdc3cf789b1facf803
Jake Waitze <jake@waitze.net>
2016-07-21 16:04:28 -0400

    Added homepage recent posts content, edited resume

--- inc/home.php
+++ inc/home.php
@@ -5,8 +5,29 @@
 		exit();
 	}
 
+	// output actual content right here rather than the static example below
+	// perhaps an about, then an <hr>, then the last three most recent blog posts...
+	// definitely label the respective sections manually.
+
 ?>
-				<h2 id="inlineElement">Welcome to my website.</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed lacus mollis, lobortis quam non, vestibulum sapien. Nam eu nibh sed velit tincidunt suscipit non vitae nunc. Suspendisse lorem sem, pharetra at erat non, suscipit gravida orci. Etiam ut ex id ligula lacinia sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec accumsan pharetra sagittis. Integer efficitur consequat mi, ut venenatis quam elementum rhoncus. Etiam sollicitudin semper mi. Ut id pulvinar tortor. Integer convallis sapien non dui porta, a aliquet enim feugiat. Phasellus vel massa non neque viverra lacinia id id augue.</p>
-				<p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque facilisis pulvinar ipsum, eu consequat justo congue eu. Fusce semper lorem nec lectus ultrices, ac vehicula turpis fringilla. Nullam imperdiet nulla augue, a iaculis mauris gravida ut. Sed commodo nulla tincidunt mauris dapibus, tempor vulputate ipsum vehicula. Quisque ac hendrerit sem. Nunc quis lacinia velit, sed dapibus nunc. Etiam sed hendrerit velit. In hac habitasse platea dictumst. Vivamus vehicula ligula a neque ultrices tincidunt. Pellentesque id dui efficitur, bibendum libero nec, semper lectus. Donec non orci vitae leo mollis tincidunt. Maecenas id purus diam. Sed sodales id tellus at congue. Sed a est scelerisque est dignissim imperdiet.</p>
-				<p>Nam auctor porta placerat. Mauris congue maximus risus eget blandit. Maecenas quis diam mauris. Etiam ut tincidunt erat. Aliquam egestas dui sed aliquam condimentum. Cras vitae metus vitae ante molestie auctor. Fusce consectetur arcu nec orci vehicula tristique. Vivamus quam mauris, efficitur eget venenatis eu, ornare quis neque. Suspendisse feugiat purus eget lectus congue ornare.</p>
+				<h1 id="inlineElement">Welcome to my personal site.</h1>
+				<p id="homeBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed lacus mollis, lobortis quam non, vestibulum sapien. Nam eu nibh sed velit tincidunt suscipit non vitae nunc. Suspendisse lorem sem, pharetra at erat non, suscipit gravida orci. Etiam ut ex id ligula lacinia sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec accumsan pharetra sagittis. Integer efficitur consequat mi, ut venenatis quam elementum rhoncus. Etiam sollicitudin semper mi. Ut id pulvinar tortor. Integer convallis sapien non dui porta, a aliquet enim feugiat. Phasellus vel massa non neque viverra lacinia id id augue.</p>
+
+<?php
+
+	$postrows = GetStoredPostsByRange($mysqli, 0, 3);
+	$total_posts_count = GetStoredPostsCount($mysqli);
+
+	if(count($postrows) == 0)
+		return; // no posts, so the below part of this page is omitted
+	else
+		echo '<hr><a href="../blog"><h1 id="recentPostsHeader">Recent Blog Posts</h1></a>';
+
+	foreach($postrows as $postrow) {
+
+		OutputBlogPost($postrow['title'], $postrow['urlkey'], $postrow['content'], $postrow['author']);
+		if($postrow != end($postrows)) // add the line if it's not the last entry
+			echo '<hr class="fadingStyleRecentPosts">';
+	}
+
+?>
